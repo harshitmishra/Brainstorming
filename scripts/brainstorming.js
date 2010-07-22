6e19b2c088c599e9fe5b4144554ffd5ed78d27a0
harshitmishra <mishrasame@gmail.com>
2010-07-22 10:17:44 +0530

    braincommit2

--- scripts/brainstorming.js
+++ scripts/brainstorming.js
@@ -1,25 +1,29 @@
 // JavaScript Document
 
 		var count=1;
-		var jarray=new Array(100);
+		var jsarr=new Array();
 	function init(){
 		var text1 = new Element('input', {id:'text', size: '30'});
         var add = new Element('button', {'class': 'add', id:'addl',value:'ADD'});
 		var fini = new Element('button', {'class': 'finish', id:'fin',value:'FINISH'});
+		var prin = new Element('button', {'class': 'prin', id:'prin',value:'Show'});
+		var load1 = new Element('button', {'class': 'load', id:'load',value:'Load'});
 	    $("data-entry").appendChild(text1);
 	    $("data-entry").appendChild(add);
       	$("data-entry").appendChild(fini);
+		$("data-entry").appendChild(prin);
+		$("data-entry").appendChild(load1);
 		text1.activate();
         $("addl").observe('click',function() {
-		//alert(document.getElementById("text").value);
-		//var div = new Element('div',{'class': 'divi',id:'div_data_entry_'+count});
+		
 		if(document.getElementById("text").value!=""){
 		var divt = document.createElement("div");
 		divt.id = "div"+count;
 		divt.className ="divi";
 		divt.innerHTML = document.getElementById("text").value;
-		jarray[count]=document.getElementById("text").value;
+		jsarr[count]=document.getElementById("text").value;
 		divt.contentEditable='true';
+		
 		var b1=document.createElement('input');
                 var b1Id='b'+count+'1';
                 b1.type='image';
@@ -27,6 +31,7 @@
 				b1.value='X';
                 b1.height='20';
                 b1.width='20';
+				
 				b1.setAttribute('id', b1Id);
                 b1.onclick=removeElement;
 		
@@ -42,19 +47,56 @@
 			document.getElementById('data-entry').removeChild(document.getElementById('text'));
             document.getElementById('data-entry').removeChild(document.getElementById('addl'));
 			document.getElementById('data-entry').removeChild(document.getElementById('fin'));
-			alert(jarray);
-			/*var fso = new ActiveXObject("Scripting.FileSystemObject");
-            varFileObject = fso.OpenTextFile("D:\\harshit.txt", 2, true,0); // 2=overwrite, true=create if not exist, 0 = ASCII
-			varFileObject.write("hsgha");
-			varFileObject.close();*/
-			jarray.toJSON();
-			alert(jarray);
 			//d.removeChild(document.getElementById("data));
 									  
 									  });
+	$("prin").observe('click',function(){
+									   if(jsarr!="")
+									   {
+									   
+									   jsarr.toJSON();
+									   alert(jsarr+"\n JSON CREATED !!!!");
+									   }
+									   else
+									   {
+										   alert("Please do some Brainstorming session before creating JSON..");
+									   }
+									   });
+	$("load").observe('click',function(){
+									  // var newJ=jsarr.toJSON().evalJSON();
+									   //alert(newJ);
+									   var name=prompt("Please enter the saved JSON","");
+									   var mySplit = name.split(",");
+									   count=1;
+									   for(i=0;i<mySplit.length;i++)
+									   {
+										   var divt = document.createElement("div");
+											divt.id = "div"+count;
+											divt.className ="divi";
+											divt.innerHTML = mySplit[i];
+											jsarr[count]=mySplit[i];
+											divt.contentEditable='true';
+											
+											var b1=document.createElement('input');
+													var b1Id='b'+count+'1';
+													b1.type='image';
+													b1.src='cross.gif';
+													b1.value='X';
+													b1.height='20';
+													b1.width='20';
+													
+													b1.setAttribute('id', b1Id);
+													b1.onclick=removeElement;
+											
+											//$("div_data_entry_"+count).innerHTML="shdjf";
+											divt.appendChild(b1);
+											document.getElementById('new').appendChild(divt);
+											count++;
+											document.getElementById("text").value="";
+									   }
+									   });
+									 
 									  
-									  
-		
 		}
 	function removeElement()
         {
